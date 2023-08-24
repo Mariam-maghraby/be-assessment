dfb01a91e39cbefc33581601c63884c1a05fc64e
Mariam-maghraby <61349498+Mariam-maghraby@users.noreply.github.com>
2023-08-24 23:48:08 +0300

    feat: added delete user methods

--- app/lib/handlers.js
+++ app/lib/handlers.js
@@ -5,129 +5,196 @@ const helpers = require('./helpers');
 const handlers = {};
 
 handlers.users = (data, callback) => {
-    const acceptableMethods = ['post', 'get', 'put', 'delete'];
-    if(acceptableMethods.indexOf(data.method) > -1){
-        handlers._users[data.method](data, callback);
-    } else {
-        callback(405);
-    }
+  const acceptableMethods = ['post', 'get', 'put', 'delete'];
+  if (acceptableMethods.indexOf(data.method) > -1) {
+    handlers._users[data.method](data, callback);
+  } else {
+    callback(405);
+  }
 };
 
 handlers._users = {};
 
 /* Post a new user */
 handlers._users.post = (data, callback) => {
-    let firstName = typeof(data.payload.firstName) == 'string' && data.payload.firstName.trim().length > 0 ? data.payload.firstName.trim() : false;
-    let lastName = typeof(data.payload.lastName) == 'string' && data.payload.lastName.trim().length > 0 ? data.payload.lastName.trim() : false;
-    let phone = typeof(data.payload.phone) == 'string' && data.payload.phone.trim().length > 0 ? data.payload.phone.trim() : false;
-    let password = typeof(data.payload.password) == 'string' && data.payload.password.trim().length > 0 ? data.payload.password.trim() : false;
-    let tosAgreement = typeof(data.payload.tosAgreement) == 'boolean' && data.payload.tosAgreement == true ? true : false;
+  let firstName = typeof (data.payload.firstName) == 'string' && data.payload.firstName.trim().length > 0 ? data.payload.firstName.trim() : false;
+  let lastName = typeof (data.payload.lastName) == 'string' && data.payload.lastName.trim().length > 0 ? data.payload.lastName.trim() : false;
+  let phone = typeof (data.payload.phone) == 'string' && data.payload.phone.trim().length > 0 ? data.payload.phone.trim() : false;
+  let password = typeof (data.payload.password) == 'string' && data.payload.password.trim().length > 0 ? data.payload.password.trim() : false;
+  let tosAgreement = typeof (data.payload.tosAgreement) == 'boolean' && data.payload.tosAgreement == true ? true : false;
 
-    if(firstName && lastName && phone && password && tosAgreement){
-        _data.read('users', phone, (err, data) => {
-            if(err){
-                let hashedPassword = helpers.hash(password);
-                if(hashedPassword){
-                    let userObject = {
-                        'firstName' : firstName,
-                        'lastName' : lastName,
-                        'phone' : phone,
-                        'hashedPassword' : hashedPassword,
-                        'tosAgreement' : true
-                    };
-                    _data.create('users', phone, userObject, (err) => {
-                        if(!err){
-                            callback(200);
-                        } else {
-                            console.log(err);
-                            callback(500, {'Error' : 'Could not create the new user'});
-                        }
-                    });
-                } else {
-                    callback(500, {'Error' : 'Could not hash the user\'s password'});
-                }
+  if (firstName && lastName && phone && password && tosAgreement) {
+    _data.read('users', phone, (err, data) => {
+      if (err) {
+        let hashedPassword = helpers.hash(password);
+        if (hashedPassword) {
+          let userObject = {
+            'firstName': firstName,
+            'lastName': lastName,
+            'phone': phone,
+            'hashedPassword': hashedPassword,
+            'tosAgreement': true
+          };
+          _data.create('users', phone, userObject, (err) => {
+            if (!err) {
+              callback(200);
             } else {
-                callback(400, {'Error' : 'A user with that phone number already exists'});
+              console.log(err);
+              callback(500, { 'Error': 'Could not create the new user' });
             }
-        });
-    }
+          });
+        } else {
+          callback(500, { 'Error': 'Could not hash the user\'s password' });
+        }
+      } else {
+        callback(400, { 'Error': 'A user with that phone number already exists' });
+      }
+    });
+  }
 };
 
 /* Get a user */
 handlers._users.get = (data, callback) => {
-    const phone = typeof(data.queryStringObject.phone) == 'string' && data.queryStringObject.phone.trim().length > 0 ? data.queryStringObject.phone.trim() : false;
-    if(phone){
-        const token = typeof(data.headers.token) == 'string' ? data.headers.token : false;
-        handlers.tokens.verifyToken(token, phone, (tokenIsValid) => {
-            if(tokenIsValid){
-                _data.read('users', phone, (err, data) => {
-                    if(!err && data){
-                        delete data.hashedPassword;
-                        callback(200, data);
-                    } else {
-                        callback(404);
-                    }
-                });
-            } else {
-                callback(403, {'Error' : 'Missing required token in header, or token is invalid'});
-            }
+  const phone = typeof (data.queryStringObject.phone) == 'string' && data.queryStringObject.phone.trim().length > 0 ? data.queryStringObject.phone.trim() : false;
+  if (phone) {
+    const token = typeof (data.headers.token) == 'string' ? data.headers.token : false;
+    handlers.tokens.verifyToken(token, phone, (tokenIsValid) => {
+      if (tokenIsValid) {
+        _data.read('users', phone, (err, data) => {
+          if (!err && data) {
+            delete data.hashedPassword;
+            callback(200, data);
+          } else {
+            callback(404);
+          }
         });
-        
-    
-    }
+      } else {
+        callback(403, { 'Error': 'Missing required token in header, or token is invalid' });
+      }
+    });
+
+
+  }
 }
 
 
 handlers._users.put = (data, callback) => {
 
-    const phone = typeof(data.payload.phone) == 'string' && data.payload.phone.trim().length > 0 ? data.payload.phone.trim() : false;
-    const firstName = typeof(data.payload.firstName) == 'string' && data.payload.firstName.trim().length > 0 ? data.payload.firstName.trim() : false;
-    const lastName = typeof(data.payload.lastName) == 'string' && data.payload.lastName.trim().length > 0 ? data.payload.lastName.trim() : false;
-    const password = typeof(data.payload.password) == 'string' && data.payload.password.trim().length > 0 ? data.payload.password.trim() : false;
-    if(phone){
-        if(firstName || lastName || password){
-            const token = typeof(data.headers.token) == 'string' ? data.headers.token : false;
-            handlers.tokens.verifyToken(token, phone, (tokenIsValid) => {
-                if(tokenIsValid){
-                    _data.read('users', phone, (err, userData) => {
-                        if(!err && userData){
-                        /*update any of these fields*/
-                        if(firstName){
-                            userData.firstName = firstName;
-                        }
-                        if(lastName){
-                            userData.lastName = lastName;
-                        }
-                        if(password){
-                            userData.hashedPassword = helpers.hash(password);
-                        }
-                        _data.update('users', phone, userData, (err) => {
-                            if(!err){
-                                callback(200);
-                            } else {
-                                console.log(err);
-                                callback(500, {'Error' : 'Could not update the user'});
-                            }
-                        })
-                    } else {
-                        callback(400, {'Error' : 'The specified user does not exist'});
-                      }
-                        
-                    });
-                } else {
-                    callback(403, {'Error' : 'Missing required token in header, or token is invalid'});
-                  }
-                });
-          
-              } else {
-                callback(400, {'Error' : 'Missing fields to update'});
+  const phone = typeof (data.payload.phone) == 'string' && data.payload.phone.trim().length > 0 ? data.payload.phone.trim() : false;
+  const firstName = typeof (data.payload.firstName) == 'string' && data.payload.firstName.trim().length > 0 ? data.payload.firstName.trim() : false;
+  const lastName = typeof (data.payload.lastName) == 'string' && data.payload.lastName.trim().length > 0 ? data.payload.lastName.trim() : false;
+  const password = typeof (data.payload.password) == 'string' && data.payload.password.trim().length > 0 ? data.payload.password.trim() : false;
+  if (phone) {
+    if (firstName || lastName || password) {
+      const token = typeof (data.headers.token) == 'string' ? data.headers.token : false;
+      handlers.tokens.verifyToken(token, phone, (tokenIsValid) => {
+        if (tokenIsValid) {
+          _data.read('users', phone, (err, userData) => {
+            if (!err && userData) {
+              /*update any of these fields*/
+              if (firstName) {
+                userData.firstName = firstName;
               }
-          
+              if (lastName) {
+                userData.lastName = lastName;
+              }
+              if (password) {
+                userData.hashedPassword = helpers.hash(password);
+              }
+              _data.update('users', phone, userData, (err) => {
+                if (!err) {
+                  callback(200);
+                } else {
+                  console.log(err);
+                  callback(500, { 'Error': 'Could not update the user' });
+                }
+              })
             } else {
-              callback(400, {'Error' : 'Missing required field'});
+              callback(400, { 'Error': 'The specified user does not exist' });
             }
+
+          });
+        } else {
+          callback(403, { 'Error': 'Missing required token in header, or token is invalid' });
+        }
+      });
+
+    } else {
+      callback(400, { 'Error': 'Missing fields to update' });
+    }
+
+  } else {
+    callback(400, { 'Error': 'Missing required field' });
+  }
+}
+
+/* Delete a user */
+handlers._users.delete = function (data, callback) {
+  const phone = typeof (data.queryStringObject.phone) == 'string' && data.queryStringObject.phone.trim().length == 10 ? data.queryStringObject.phone.trim() : false;
+  if (phone) {
+    const token = typeof (data.headers.token) == 'string' ? data.headers.token : false;
+    handlers._tokens.verifyToken(token, phone, function (tokenIsValid) {
+      if (tokenIsValid) {
+        _data.read('users', phone, function (err, userData) {
+          if (!err && userData) {
+            _data.delete('users', phone, function (err) {
+              if (!err) {
+                const userChecks = typeof (userData.checks) == 'object' && userData.checks instanceof Array ? userData.checks : [];
+                const checksToDelete = userChecks.length;
+                if (checksToDelete > 0) {
+                  let checksDeleted = 0;
+                  let deletionErrors = false;
+                  userChecks.forEach(function (checkId) {
+                    _data.delete('checks', checkId, function (err) {
+                      if (err) {
+                        deletionErrors = true;
+                      }
+                      checksDeleted++;
+                      if (checksDeleted == checksToDelete) {
+                        if (!deletionErrors) {
+                          callback(200);
+                        } else {
+                          callback(500, { 'Error': 'Errors encountered while attempting to delete all of the user\'s checks. All checks may not have been deleted from the system successfully.' })
+                        }
+                      }
+                    });
+                  });
+                } else {
+                  callback(200);
+                }
+              } else {
+                callback(500, { 'Error': 'Could not delete the specified user' });
+              }
+            });
+          } else {
+            callback(400, { 'Error': 'Could not find the specified user' });
           }
-                
-                   
+        });
+      } else {
+        callback(403, { 'Error': 'Missing required token in header, or token is invalid' });
+      }
+    });
+  } else {
+    callback(400, { 'Error': 'Missing phone number field (required)' });
+  }
+};
+
+
+
+handlers.ping = (data, callback) => {
+  callback(200);
+};
+
+/*Not found handler*/
+handlers.notFound = (data, callback) => {
+  callback(404);
+};
+
+
+
+module.exports = handlers;
+
+
+
 
 
