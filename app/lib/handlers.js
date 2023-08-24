6b0cd3701184a3b8b63ee6ca066ed239cb102e07
Mariam-maghraby <61349498+Mariam-maghraby@users.noreply.github.com>
2023-08-24 23:30:10 +0300

    feat: added get and put user methods

--- app/lib/handlers.js
+++ app/lib/handlers.js
@@ -52,3 +52,82 @@ handlers._users.post = (data, callback) => {
         });
     }
 };
+
+/* Get a user */
+handlers._users.get = (data, callback) => {
+    const phone = typeof(data.queryStringObject.phone) == 'string' && data.queryStringObject.phone.trim().length > 0 ? data.queryStringObject.phone.trim() : false;
+    if(phone){
+        const token = typeof(data.headers.token) == 'string' ? data.headers.token : false;
+        handlers.tokens.verifyToken(token, phone, (tokenIsValid) => {
+            if(tokenIsValid){
+                _data.read('users', phone, (err, data) => {
+                    if(!err && data){
+                        delete data.hashedPassword;
+                        callback(200, data);
+                    } else {
+                        callback(404);
+                    }
+                });
+            } else {
+                callback(403, {'Error' : 'Missing required token in header, or token is invalid'});
+            }
+        });
+        
+    
+    }
+}
+
+
+handlers._users.put = (data, callback) => {
+
+    const phone = typeof(data.payload.phone) == 'string' && data.payload.phone.trim().length > 0 ? data.payload.phone.trim() : false;
+    const firstName = typeof(data.payload.firstName) == 'string' && data.payload.firstName.trim().length > 0 ? data.payload.firstName.trim() : false;
+    const lastName = typeof(data.payload.lastName) == 'string' && data.payload.lastName.trim().length > 0 ? data.payload.lastName.trim() : false;
+    const password = typeof(data.payload.password) == 'string' && data.payload.password.trim().length > 0 ? data.payload.password.trim() : false;
+    if(phone){
+        if(firstName || lastName || password){
+            const token = typeof(data.headers.token) == 'string' ? data.headers.token : false;
+            handlers.tokens.verifyToken(token, phone, (tokenIsValid) => {
+                if(tokenIsValid){
+                    _data.read('users', phone, (err, userData) => {
+                        if(!err && userData){
+                        /*update any of these fields*/
+                        if(firstName){
+                            userData.firstName = firstName;
+                        }
+                        if(lastName){
+                            userData.lastName = lastName;
+                        }
+                        if(password){
+                            userData.hashedPassword = helpers.hash(password);
+                        }
+                        _data.update('users', phone, userData, (err) => {
+                            if(!err){
+                                callback(200);
+                            } else {
+                                console.log(err);
+                                callback(500, {'Error' : 'Could not update the user'});
+                            }
+                        })
+                    } else {
+                        callback(400, {'Error' : 'The specified user does not exist'});
+                      }
+                        
+                    });
+                } else {
+                    callback(403, {'Error' : 'Missing required token in header, or token is invalid'});
+                  }
+                });
+          
+              } else {
+                callback(400, {'Error' : 'Missing fields to update'});
+              }
+          
+            } else {
+              callback(400, {'Error' : 'Missing required field'});
+            }
+          }
+                
+                   
+
+
